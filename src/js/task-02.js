0e1093b1bd3925521681638de97bae35cee975f4
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-11-28 20:59:07 +0200

    Task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listOfEl = document.querySelector('#ingredients')
+
+ingredients.forEach(ingredient => {
+  const itemOfEl = document.createElement('li')
+  itemOfEl.classList.add('item')
+  itemOfEl.textContent = ingredient;
+  listOfEl.append(itemOfEl)
+})
+
+console.log(listOfEl)
