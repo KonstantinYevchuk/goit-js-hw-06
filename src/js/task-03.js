568756c102830df88f9be0f6ee71c4fa3176750c
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-11-30 14:07:36 +0200

    update task3

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -13,12 +13,15 @@ const images = [
   },
 ];
 const listGallery = document.querySelector('.gallery')
+// listGallery.style.listStyle = 'none';
 const imagesEl = images
-.map(image => `<li class="list_item"> <img url='${image.url}, alt='${image.alt}'></li>`)
-.join("")
+.map(({url, alt}) => `<li class="list_item">
+<img src="${url}", alt= "${alt}", width = "420", height= "300"/>
+</li>`)
+.join("");
 listGallery.insertAdjacentHTML("afterbegin", imagesEl)
 console.log(imagesEl)
 
-const styles = `<style> .gallery { display: flex; justify-content: space-around; align-items: center } </style>`;
+const styles = `<style> .gallery { display: flex; justify-content: space-around; align-items: center; list-style: none } </style>`;
 const headEl = document.querySelector("link");
 headEl.insertAdjacentHTML("afterend", styles)
