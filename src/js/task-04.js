21332303dea4231d96511d8c5ce0ff55e31d0462
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-11-30 15:17:11 +0200

    Task4

---
+++ src/js/task-04.js
@@ -0,0 +1,43 @@
+const counterValue = document.querySelector('#value');
+console.log(counterValue);
+
+const counter = {
+    value: 0,
+    decrement() {
+        this.value -= 1;
+    },
+    increment() {
+        this.value += 1;
+    }
+}
+const handleClickDecrement = () => {
+    counter.decrement();
+    counterValue.textContent = counter.value;
+}
+const handleClickIncrement = () => {
+    counter.increment();
+    counterValue.textContent = counter.value;
+}
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+
+decrementBtn.addEventListener("click", handleClickDecrement);
+incrementBtn.addEventListener("click", handleClickIncrement);
+
+
+// let total = 0;
+// const handleClickDecrement = () => {
+    
+//     if (total -= 1) {
+//        counterValue.textContent = total; 
+//     };
+//     console.log(total)
+// }
+// const handleClickIncrement = () => {
+//     if (total += 1) {
+//         counterValue.textContent = total
+//     };
+//     console.log(total)
+// }
