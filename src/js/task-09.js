0c3d3b4c5fe492b44668ab22c47e2f46e0e40a83
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2022-12-03 13:06:27 +0200

    Update Task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,21 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+const btnEl = document.querySelector('.change-color');
+const textEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const handleClick = (evt) => {
+  console.log(evt.currentTarget.value)
+  const randomColor = bodyEl.style.backgroundColor = getRandomHexColor() 
+  textEl.textContent = randomColor
+}
+
+btnEl.addEventListener("click", handleClick)
+
